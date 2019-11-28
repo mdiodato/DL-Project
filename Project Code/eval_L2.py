15d7c5b010017a08992faa1a997b879458b917ba
mdiodato <michael.diodato@gmail.com>
2019-11-28 01:20:48 +0000

    Updated eval_L2.py

--- Project Code/eval_L2.py
+++ Project Code/eval_L2.py
@@ -5,15 +5,13 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 
 import os
 from collections import OrderedDict
-
-import data
+import numpy as np
 from options.test_options import TestOptions
-from models.pix2pix_model import Pix2PixModel
-from util.visualizer import Visualizer
-from util import html
 import pandas as pd
-import PIL.Image
+from PIL import Image
+import math
 from data.image_folder import make_dataset
+from collections import defaultdict
 
 def save_file_path(opt, makedir=False):
     expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
@@ -27,31 +25,56 @@ opt = TestOptions().parse()
 
 load_data = pd.read_csv(save_file_path(opt) + '.csv')
 #dataloader = data.create_dataloader(opt)
+print('Loaded information data file')
 
-test_images_folder = './results/' + opt.name + '/test_' + opt.which_epoch + '/images/' + opt.real_label + '/synthesized_image/'
-test_images = make_dataset(test_images_folder)
+test_images_folder = './results/' + opt.name + '/test_' + str(opt.which_epoch) + '/images/'
+test_images = make_dataset(test_images_folder, recursive=True)
+test_images_all = [x for x in test_images if 'synthesized_image' in x]
+print('Loaded test images')
 
-res = {}
-for test_image in test_images:
-    test_im = Image.open(test_image)
-    test_im = test_im.convert('RGB')
-    rows,cols,colors = test_im.shape
-    img_size = rows*cols*colors
-    test_im = test_im.reshape(img_size)
-    
-    res_test = []
+res = defaultdict(list)
+batch_count = 0
+for j in range(0, len(test_images_all), opt.batchSize):
+    print("Working on batch coount", batch_count+1, ' of ', len(range(0, len(test_images_all), opt.batchSize)))
+    test_arrs = []
+    test_images = test_images_all[j:j+opt.batchSize]
+    for test_image in test_images:
+        #print('Working on ', test_image)
+        test_im = Image.open(test_image)
+        test_im = test_im.convert('RGB')
+        test_im = np.array(test_im)
+        rows,cols,colors = test_im.shape
+        img_size = rows*cols*colors
+        test_im = test_im.ravel()
+        test_arrs.append(test_im)
+        
+    count = 0
     for real_image in load_data['image_paths']:
+        if count % math.floor(len(load_data['image_paths'])/10) == 0:
+            print('Processed ', count, ' Images of ', len(load_data['image_paths']))
         real_im = Image.open(real_image)
         real_im = real_im.convert('RGB')
         real_im = real_im.resize((rows, cols), Image.BICUBIC)
+        real_im = np.array(real_im)
         rows,cols,colors = real_im.shape
         img_size = rows*cols*colors
         real_im = real_im.reshape(img_size)
-        
-        l2 = np.linalg.norm(test_im - real_im)
-        
-    res[test_image] = res_test
+        count += 1
 
-pd.DataFrame([res], index = load_data['image_paths'])
+        for i in range(len(test_images)):
+            test_im = test_arrs[i]
+            test_image = test_images[i]
+            l2 = np.linalg.norm(test_im - real_im)
+            
+            res[test_image].append(l2)
+    batch_count += 1
+
+print('Saving results')
+res = pd.DataFrame.from_dict(res,orient='index').transpose() 
+res.index = list(load_data['image_paths'])
 
 res.to_csv('./results/' + opt.name + '/test_' + opt.which_epoch + '/L2 Comparison.csv')
+
+with pd.ExcelWriter('./results/' + opt.name + '/test_' + opt.which_epoch + '/L2 Comparison.xlsx') as writer:  # doctest: +SKIP
+    res.to_excel(writer, sheet_name='L2 Results')
+    load_data.to_excel(writer, sheet_name='Dataloader')
